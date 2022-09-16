ce987aac2901819522c8b23db7d247314ee2f389
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-16 19:26:07 +0500

    add Zadacha 29

---
+++ Zadacha 29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Пользователь вводит 8 чисел.
+// Поместите их в массив и распечатайте.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+double[] InputArrayListNumber(int lenghtMassiv)
+{
+    double[] numList = new double[lenghtMassiv];
+    for (int i = 0; i < lenghtMassiv; i++)
+    {
+        while(true)
+        {
+            try
+            {
+                Console.Write($"Nomer {i+1}. Введите число: ");
+                numList[i] = Convert.ToDouble(Console.ReadLine());
+                break;
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine($"Nomer {i+1}. Ошибка!");
+            }
+        }
+    }
+    return numList;
+}
+
+double[] resultNumberList = InputArrayListNumber(8);
+Console.WriteLine(string.Join(", ", resultNumberList));
