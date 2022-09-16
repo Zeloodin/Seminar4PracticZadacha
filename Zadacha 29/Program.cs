5ad5241129e03075cf2f3bd4cdaad3a1b34cdf9c
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-16 20:46:56 +0500

    Update Zadacha 29

--- Zadacha 29/Program.cs
+++ Zadacha 29/Program.cs
@@ -3,36 +3,17 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int InputNumberLenghtMassiv()
+double[] InputArrayListNumber(int lenghtMassiv)
 {
-    while(true)
-    {
-        try
-        {
-            Console.Write("Введите длину массива: ");
-            int NumberLenghtMassiv = Convert.ToInt16(Console.ReadLine());
-            return NumberLenghtMassiv;
-            break;
-        }
-        catch(System.FormatException)
-        {
-            Console.WriteLine("Ошибка!");
-        }
-    }
-}
-
-static extern double[] InputStringNumber_OutListNumber(int len);
-{
-    int lenght = len;
-    double[] numberStringList = new double[lenght];
-    for (int i = 0; i < lenght; i++)
+    double[] numList = new double[lenghtMassiv];
+    for (int i = 0; i < lenghtMassiv; i++)
     {
         while(true)
         {
             try
             {
                 Console.Write($"Nomer {i+1}. Введите число: ");
-                numberStringList[i] = Convert.ToDouble(Console.ReadLine());
+                numList[i] = Convert.ToDouble(Console.ReadLine());
                 break;
             }
             catch(System.FormatException)
@@ -41,56 +22,8 @@ static extern double[] InputStringNumber_OutListNumber(int len);
             }
         }
     }
-    return numberStringList;
-}
-
-double[] InputArrayListNumber(double[] numberArray)
-{
-    double[] numList = new double[numberArray.Length];
-    
-    
-    for (int i = 0; i < numberArray.Length; i++)
-    {
-        bool true1 = true;
-        while(true1)
-        {
-            try
-            {
-                numList[i] = numberArray[i];
-                true1 = false;
-            }
-            catch(System.FormatException)
-            {
-                bool true2 = true;
-                while(true2)
-                {
-                    try
-                    {
-                        Console.WriteLine($"Nomer {i+1}. Ошибка!");
-                        Console.Write($"Nomer {i+1}. Введите число: ");
-                        numList[i] = Convert.ToDouble(Console.ReadLine());
-                        true2 = false;
-                    }
-                    catch(System.FormatException)
-                    {
-                    }
-                }
-                
-            }
-        }
-    }
     return numList;
 }
 
-
-
-
-int lenghtList = InputNumberLenghtMassiv();
-
-// double[] NumStringList = InputStringNumber_OutListNumber(lenghtList);
-
-// double[] resultNumberList = InputArrayListNumber(NumStringList);
-
-double[] resultNumberList = InputStringNumber_OutListNumber(lenghtList);
-
+double[] resultNumberList = InputArrayListNumber(8);
 Console.WriteLine(string.Join(", ", resultNumberList));
